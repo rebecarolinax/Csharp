7ff412cb1d07d758abfa9e9ae8df0d58b6a4e6fa
Rebeca Carolina Fantini Furini De Lima <54931473865@info.local>
2023-10-31 14:40:08 -0300

    :heavy_check_mark: Csharp

--- classes-estáticas/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿using classes_estaticas;
-
- float result =  ConversorTemperatura.CelsiusParaFahreheint(24);
-
- Console.WriteLine($"{result}");
- 
- float results = ConversorTemperatura.FahreinheitParaCelsius(70);
- Console.WriteLine($"{results}");
--- exercícios-extras/ex-3/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿Console.WriteLine($"Bem vindo! Por favor digite o raio do círculo:");
-float raio = float.Parse(Console.ReadLine()!);
-float diametro = (raio + raio);
-double area = Math.Round((Math.PI * (raio * raio)), 2);
-
-Console.WriteLine($"Bom, se seu círculo possui esse raio, seu diâmetro é de {diametro} e sua área é de {area}");
