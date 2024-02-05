3744c82d952b91c44315091aee924376a9c6137d
Rebeca <rebecacarolina75@gmail.com>
2024-02-05 14:55:32 -0300

    Csharp

--- decremento-exercícios/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿for(int x = 500; x >= 0; x -= 5)
-{
-    Console.WriteLine(x);
-}
-
--- métodos-construtores/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using construtores;
-
-// Produto p = new Produto();
-
-// p.Codigo = "1234";
-
-// Console.WriteLine($"{p.Codigo}");
-
-Produto p2 = new Produto("2000","celular",1.78f);
